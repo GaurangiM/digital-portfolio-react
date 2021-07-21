5f410a5e5cfcd0b5607aacd00cf154f7420d17fc
GaurangiM <manegaurangi89@gmail.com>
2021-07-21 21:53:13 +0200

    Build about me page and add styling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import aboutImg from '../../assets/about-me.gif'
 import './About.css'
 
-const About = ()=> {
+const About = () => {
   return (
     <div className="section-container">
-      About page
+      <Header heading="About me"
+        details="Hello all, welcome to my portfolio website!" />
+      <div className="about-main">
+        <div className="about-main-left">
+      
+            <img src={aboutImg} alt="about me" className="about-vector" />
+          
+        </div>
+        <div className="about-main-right">
+          <p>Hello all, welcome to my portfolio profile! I am passionate about front-end technologies and have been following my passion through different online courses. Recently, I graduated from an intensive 12-weeks long Full stack developer bootcamp from Codaisseur. You can have a look at projects I built over here.</p>
+        </div>
+      </div>
+      <Footer phrase="Check out my projects !"
+        toAddress="/projects"
+      />
+
     </div>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './Footer.css'
+
+const Footer = ({phrase, toAddress})=> {
+  return (
+    <div className="footer-link">
+      <Link to={toAddress} className="footer-link-element" >
+        {phrase}
+      </Link>
+    </div>
+  )
+}
+
+export default Footer
