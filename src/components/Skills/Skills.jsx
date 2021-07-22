1c31103cb940b9d296947c58edb0fe7cf44b647c
GaurangiM <manegaurangi89@gmail.com>
2021-07-22 12:15:24 +0200

    Add header, footer to skills page

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
 import './Skills.css'
 
 const Skills = ()=> {
   return (
     <div className="section-container">
-      Skills page
+      <Header heading="My Skills"
+              details="I am always on a journey of learning new skills, here are the skills I have learnt so far !"/>
+      <Footer phrase="Get in touch !"
+              toAddress="/contact"/>
     </div>
   )
 }
