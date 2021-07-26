5c3d4b734446014e58878599d955cd642bc99a65
GaurangiM <manegaurangi89@gmail.com>
2021-07-26 15:43:59 +0200

    Add bottom margin for footer

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
-import skillsAnime from '../../assets/skills-anime.gif'
+import skillsVector from './../../assets/skills_vector.png';
 import { skillList } from '../../assets/skillsData'
 import SkillCard from './SkillCard'
 import './Skills.css'
@@ -12,24 +12,25 @@ const Skills = () => {
     <div className="section-container">
       <Header heading="My Skills"
         details="I am always on a journey of learning new skills, here are the skills I have learnt so far !" />
-      <div className="skills-page-container">
-        <div className="skills-vector-frame">
-          <img src={skillsAnime} alt="skills anime" className="skills-vector" />
-        </div>
-        <div className="skill-card-container">
-          {
-            skillList.map(skill => {
-              return <SkillCard skillName={skill.skillName}
-                skillUrl={skill.skillUrl} />
-            })
-          }
-        </div>
 
+      <div className="skill-card-container">
+        {
+          skillList.map(skill => {
+            return <SkillCard skillName={skill.skillName}
+              skillUrl={skill.skillUrl} />
+          })
+        }
       </div>
 
       <Footer phrase="Get in touch !"
         toAddress="/contact" />
-
+      <div className='skills-vector-frame'>
+        <img
+          src={skillsVector}
+          alt='skills'
+          className='skills-vector'
+        />
+      </div>
     </div>
   )
 }
